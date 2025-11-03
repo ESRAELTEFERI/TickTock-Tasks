b7576081b7018af7dfef81452fd0823305efa990
Esrael Teferi <teferiesrael21@gmail.com>
2025-11-03 11:08:04 +0300

    update

--- client/client-app/src/app/app.component.spec.ts
+++ client/client-app/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'client-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client-app');
+    expect(app.title).toEqual('TickTok-Task');
   });
 
   it('should render title', () => {
